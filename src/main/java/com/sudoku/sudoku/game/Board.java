56088acc53a32470cf38901bae274a6b23fb05bb
MaksimsALV <maxim.ahmetov@gmail.com>
2025-06-20 15:05:21 +0300

    board class

---
+++ src/main/java/com/sudoku/sudoku/game/Board.java
@@ -0,0 +1,13 @@
+package com.sudoku.sudoku.game;
+
+
+//todo starting with 3x3 then make it 9x9
+
+
+public class Board {
+   public static Integer[][] board = {
+           {null, null, null},
+           {null, null, null},
+           {null, null, null}
+   };
+}
